bb89ba6a4152390c5bc26cf9063d465805fe879a
nabati17 <asnaba93@gmai.com>
2023-03-29 00:28:18 +0700

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,34 @@
+import 'regenerator-runtime';
+import '../styles/main.css';
+import '../styles/responsive.css';
+
+import('../DATA.json').then(({ default: jsonData }) => {
+  console.log(jsonData);
+  let datas = jsonData['restaurants'];
+  let dataList = '';
+  datas.forEach(function (data) {
+    dataList += `
+        <article class="post-item">
+          <img class="image-tumbnail" src="${data.pictureId}" alt="" />
+          <div class="post-item__content">
+            <h1 class="post-item__title">${data.name}</h1>
+            <p class="post-item__date">Location In ${data.city} <br /> Rating ${data.rating}</p>
+            <p class="post-item__description">${data.description}</p>
+          </div>
+        </article>
+        `;
+  });
+  document.querySelector('.posts').innerHTML = dataList;
+});
+
+//navbar
+let menu = document.getElementById('menu-icon');
+let list = document.querySelector('.list');
+
+menu.addEventListener('click', function () {
+  list.classList.toggle('active');
+});
+
+window.onscroll = () => {
+  list.classList.remove('active');
+};
